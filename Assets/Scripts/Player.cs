729bc26df4e0af579432a14e71574bb491b882ef
Brandon <brandonbaylis@gmail.com>
2020-11-06 22:06:10 +1100

    WIP player paddle pull-back

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
 
 public class Player : MonoBehaviour
 {
@@ -7,15 +8,35 @@ public class Player : MonoBehaviour
 
     public KeyCode moveLeft;
     public KeyCode moveRight;
+    public KeyCode pullBack;
 
     public float moveDirection;
 
-    // Update is called once per frame
-    private void FixedUpdate()
-    {
-        float minX = Camera.main.ScreenToWorldPoint(new Vector3(0 + 55, 0)).x;
-        float maxX = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width - 55, 0)).x;
+    //Min and Max player x movement positions
+    private float minX;
 
+    private float maxX;
+
+    private float minY;
+    private float maxY;
+
+    public void Start()
+    {
+        minX = Camera.main.ScreenToWorldPoint(new Vector3(0 + 55, 0)).x;
+        maxX = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width - 55, 0)).x;
+    }
+
+    public void SetClampY(float passedY)
+    {
+        minY = passedY;
+        maxY = passedY * 1.5f;
+        Debug.Log(gameObject.name + " " + maxY);
+    }
+
+    // Update is called once per frame
+    public void FixedUpdate()
+    {
+        //Movement code
         if (Input.GetKey(moveRight))
         {
             rb.position = new Vector2(Mathf.Clamp(rb.position.x, minX, maxX), rb.position.y);
@@ -31,6 +52,12 @@ public class Player : MonoBehaviour
         else
         {
             moveDirection = 0f;
+        }
+
+        if (Input.GetKey(pullBack))
+        {
+            rb.position = new Vector2(rb.position.x, Mathf.Clamp(rb.position.y, minY, maxY));
+            rb.MovePosition(new Vector2(rb.position.x, rb.position.y * 1.01f));
         }
     }
 }
